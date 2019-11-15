1d9d11fe2d282bb5a3a3f8854cb8631f43c8d9f1
codycmartin <codycmartin89@gmail.com>
2019-11-15 14:41:40 -0800

    Drag and dropped console files from my local save to the repo file. All problems complete.

---
+++ CA2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2
+{
+    class Program
+    {
+        static void Main(string[] args)            
+        {
+            
+            Console.WriteLine("The name of this program is CA2");
+            Console.WriteLine();
+
+            string s;
+            while ((s = Console.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
